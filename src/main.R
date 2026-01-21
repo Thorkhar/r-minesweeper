6d185741f0e73b620081a3346db89810c10a02b4
Bram Haverkort <b.haverkort.1@student.rug.nl>
2026-01-21 23:17:24 +0100

    minefield printing for debugging

--- src/main.R
+++ src/main.R
@@ -7,3 +7,17 @@ field_height <- 10
 source("src/class/Field.R")
 
 minefield <- Field$new(width = field_width, height = field_height)
+
+terminal_field <- ""
+for (i in seq_len(minefield$height)) {
+  for (j in seq_len(minefield$width)) {
+    if (minefield$tiles[i, j][[1]]$is_mine) {
+      terminal_field <- paste0(terminal_field, "|X")
+    } else {
+      terminal_field <- paste0(terminal_field, "| ")
+    }
+  }
+  terminal_field <- paste0(terminal_field, "|\n")
+}
+
+cat(terminal_field)
